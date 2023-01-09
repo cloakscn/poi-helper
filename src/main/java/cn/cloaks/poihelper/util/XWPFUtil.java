0a35b6bd1b8cb68bf0326bc7bc1eb891646708e1
王艺 <wangyi@fengtaisec.com>
2023-01-09 17:57:42 +0800

    更新版本

--- src/main/java/cn/cloaks/poihelper/util/XWPFUtil.java
+++ src/main/java/cn/cloaks/poihelper/util/XWPFUtil.java
@@ -2,6 +2,6 @@ package cn.cloaks.poihelper.util;
 
 public class XWPFUtil {
     public static void run() {
-        System.out.println("Running...");
+        System.out.println("Running with 1.0.1 version...");
     }
 }
